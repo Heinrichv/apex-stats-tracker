7eafd711ec46fd9065f37858e27b63526f44b3dc
Heinrich Venter <heini141@outlook.com>
2019-03-09 12:03:15 +0200

    feat(api call): added api call in express js to get stats

--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -9,7 +9,7 @@ export class StatsService {
   constructor(readonly http: HttpClient) { }
 
   getPlayerStats(platform: Number, username: String) {
-    return this.http.get(`https://public-api.tracker.gg/apex/v1/standard/profile/${platform}/${username}/`, {
+    return this.http.get(`http://localhost:4500/api/v1/stats/${platform}/${username}`, {
       headers: {
         'TRN-Api-Key': 'c2062457-01a7-4146-8a70-59d9131d070f',
         'Accept': 'application/json'
